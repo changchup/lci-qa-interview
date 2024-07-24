a23fd8ccba3309ebb55f9deaba26fa79d2d56d0d
Hamish Anderson <changchup.lodro@gmail.com>
2024-07-24 09:09:23 -0300

    First commit

---
+++ tests/to-do.spec.ts
@@ -0,0 +1,32 @@
+// Use Playwright and the utilities available within playwright to complete the test cases
+// listed below using the following example todomvc app;
+// https://todomvc.com/examples/react/dist/
+
+// Given I am a user
+// When I create a new todo item
+// Then it appears last on my todo list
+
+// Given I have marked a todo item as complete
+// When I view the Active list
+// Then only Active (Not Completed) todo items are shown
+
+// Given I have marked a todo item as complete
+// When I click “Clear Completed”
+// Then the completed todo item is removed from my todo list
+// And the todo item is moved to the Completed list
+
+
+// ************
+// ************
+// Given I have created a todo item
+// When I edit a todo item
+// Then the todo item gets updated with the new changes
+
+// Given I have created a todo item
+// When I delete a todo item using the red X
+// Then the todo item is removed from my todo list
+
+// Given I have created a todo item
+// When I mark a todo item as completed
+// Then it is marked with a green check mark
+// And it is crossed off my todo list with a Strikethrough
